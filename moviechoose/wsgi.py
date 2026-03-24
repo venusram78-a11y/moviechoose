d660ef2d5c3269d9783f6c925b94f5ebe2b2dc3c
venusram78-a11y <venusram78@gmail.com>
2026-03-24 15:19:10 +0530

    Add custom path to WSGI application

--- moviechoose/wsgi.py
+++ moviechoose/wsgi.py
@@ -1,6 +1,12 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+import sys
+
+path = '/home/venusram78/moviechoose'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moviechoose.settings.production")
 
